c5a6ead54f5acda345698a20800f426f5b5e733e
Lucas Bartholemy <luke-@users.noreply.github.com>
2023-06-01 15:48:00 +0200

    Update Events.php

--- Events.php
+++ Events.php
@@ -14,32 +14,5 @@ use Yii;
 
 class Events
 {
-    /**
-     * JWT Handling on login page
-     *
-     * @param \yii\base\ActionEvent $event
-     * @return void
-     * @since 1.1
-     */
-    public static function onAuthClientCollectionInit($event)
-    {
-        if (!Yii::$app->user->isGuest) {
-            return;
-        }
 
-        if (Yii::$app->authClientCollection->hasClient('jwt')) {
-            /** @var JWT $jwtAuth */
-            $jwtAuth = Yii::$app->authClientCollection->getClient('jwt');
-
-            if ($jwtAuth->checkIPAccess()) {
-                if ($jwtAuth->autoLogin && $event->action->id == 'login' && empty(Yii::$app->request->get('noJwt')) {
-                    $event->isValid = false;
-                    return $jwtAuth->redirectToBroker();
-                }
-            } else {
-                // Not allowed, remove authClient
-                Yii::$app->authClientCollection->removeClient('jwt');
-            }
-        }
-    }
 }
