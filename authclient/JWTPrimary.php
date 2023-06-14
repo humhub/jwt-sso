894e41bae85b84a182167dc47a1745f10bdc5fa5
Lucas Bartholemy <lucas@bartholemy.com>
2023-06-14 20:20:29 +0200

    Added JWTPrimary auth client

---
+++ authclient/JWTPrimary.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace humhub\modules\sso\jwt\authclient;
+
+use humhub\modules\user\authclient\interfaces\PrimaryClient;
+use humhub\modules\user\authclient\interfaces\AutoSyncUsers;
+use humhub\modules\user\authclient\interfaces\SyncAttributes;
+
+class JWTPrimary extends JWT implements PrimaryClient, AutoSyncUsers, SyncAttributes
+{
+    public $syncAttributes = ['email', 'username'];
+
+    public function getUser()
+    {
+        // Not supported
+        return null;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function syncUsers()
+    {
+        // Users needs to be synced manually. e.g. via REST
+        return null;
+    }
+
+    public function getSyncAttributes()
+    {
+        return $this->syncAttributes;
+    }
+}
--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -1,16 +1,23 @@
 Changelog
 =========
 
+1.1.1 (June 14, 2023)
+--------------------
+
+- Enh: Added `JWTPrimary` AuthClient for auto synced users
+
 1.1.0 (June 1, 2023)
 --------------------
+
 - Enh: Changed since v1.14 deprecated method 'AuthClientHelpers'
 - Enh: Added option to bypass forced JWT login
 
 1.0.1 (March 30, 2020)
 -------------------------
-- Enh: Documentation updates
 
+- Enh: Documentation updates
 
 1.0.0 (November 6, 2019)
 -------------------------
+
 - Enh: Initial commit of standalone version
