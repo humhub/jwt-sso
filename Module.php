47660abf6cfb9df5748d369081c2f9ec5be09614
ArchBlood <35392110+ArchBlood@users.noreply.github.com>
2024-01-11 09:04:01 -0500

    Update Module.php

--- Module.php
+++ Module.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link https://www.humhub.org/
  * @copyright Copyright (c) 2019 HumHub GmbH & Co. KG
@@ -8,10 +9,8 @@
 namespace humhub\modules\sso\jwt;
 
 use humhub\modules\sso\jwt\models\Configuration;
-use humhub\modules\user\authclient\Collection;
-use humhub\components\Event;
-use Yii;
 use yii\helpers\Url;
+use Yii;
 
 class Module extends \humhub\components\Module
 {
@@ -34,59 +33,5 @@ class Module extends \humhub\components\Module
             $this->configuration->loadBySettings();
         }
         return $this->configuration;
-    }
-
-    /**
-     * JWT Handling on login page.
-     *
-     * @param Event $event The event triggering this method.
-     * @return void
-     * @throws \yii\base\InvalidConfigException If there are configuration issues.
-     * @throws \yii\base\InvalidParamException If parameters are invalid.
-     * @throws \yii\web\ServerErrorHttpException If a server error occurs.
-     * @since 1.1
-     */
-    public static function onAuthClientCollectionInit($event)
-    {
-        try {
-            if (!Yii::$app->user->isGuest) {
-                return;
-            }
-
-            /** @var Collection $authClientCollection */
-            $authClientCollection = $event->sender;
-
-            $config = Configuration::getInstance();
-
-            if (!empty($config->enabled)) {
-                $authClientCollection->setClient('jwt', [
-                    'class' => authclient\JWT::class,
-                    'url' => $config->url,
-                    'sharedKey' => $config->sharedKey,
-                    'supportedAlgorithms' => $config->supportedAlgorithms,
-                    'idAttribute' => $config->idAttribute,
-                    'leeway' => $config->leeway,
-                    'allowedIPs' => $config->allowedIPs
-                ]);
-            }
-
-            if (isset(Yii::$app->authClientCollection) && Yii::$app->authClientCollection->hasClient('jwt')) {
-                $jwtAuth = Yii::$app->authClientCollection->getClient('jwt');
-
-                if ($jwtAuth->checkIPAccess()) {
-                    if ($jwtAuth->autoLogin && $event->action->id === 'login' && empty(Yii::$app->request->get('noJwt'))) {
-                        if ($event->isValid) {
-                            $event->isValid = false;
-                            return $jwtAuth->redirectToBroker();
-                        }
-                    }
-                } else {
-                    Yii::$app->authClientCollection->removeClient('jwt');
-                }
-            }
-        } catch (\Exception $e) {
-            // Log or handle the exception as needed
-            Yii::error('Error occurred: ' . $e->getMessage());
-        }
     }
 }
